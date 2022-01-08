0789578747f468cf73a86098f454aef53e4f67d5
Aman Rojjha <aman.rojjha@research.iiit.ac.in>
2022-01-09 00:36:22 +0530

    Added Single Node TapTree compilation!

--- src/policy/mod.rs
+++ src/policy/mod.rs
@@ -232,9 +232,12 @@ mod tests {
         super::miniscript::{context::Segwitv0, Miniscript},
         Concrete, Liftable, Semantic,
     };
-    use bitcoin;
+    use descriptor::TapTree;
+    use policy::concrete;
     use std::str::FromStr;
-    use DummyKey;
+    use std::sync::Arc;
+    use {bitcoin, Descriptor};
+    use {DummyKey, Tap};
 
     type ConcretePol = Concrete<DummyKey>;
     type SemanticPol = Semantic<DummyKey>;
@@ -363,5 +366,24 @@ mod tests {
             ),
             ms_str.lift().unwrap()
         );
+    }
+
+    #[test]
+    #[cfg(feature = "compiler")]
+    fn single_leaf_tr_compile() {
+        for k in 1..5 {
+            let unspendable_key: String = "z".to_string();
+            let policy: concrete::Policy<String> =
+                policy_str!("thresh({},pk(A),pk(B),pk(C),pk(D))", k);
+            let descriptor = policy
+                .compile_tr::<Tap>(Some(unspendable_key.clone()))
+                .unwrap();
+
+            let ms_compilation: Miniscript<String, Tap> = ms_str!("multi_a({},A,B,C,D)", k);
+            let tree: TapTree<String> = TapTree::Leaf(Arc::new(ms_compilation));
+            let expected_descriptor = Descriptor::new_tr(unspendable_key, Some(tree)).unwrap();
+
+            assert_eq!(descriptor, expected_descriptor);
+        }
     }
 }
